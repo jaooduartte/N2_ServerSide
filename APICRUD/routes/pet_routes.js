b60f434ec406065d3d4c9bab2a4fc57cf22b2126
João Duarte <jpdx2010@hotmail.com>
2024-05-25 13:59:49 -0300

    Modificações para atender ao database PETSHOP

---
+++ APICRUD/routes/pet_routes.js
@@ -0,0 +1,13 @@
+import express from "express"
+import {pet} from "../controller/pet_controller.js"
+
+let router = express.Router()
+
+router.get('/pet', pet.all);
+router.get('/pet/search', pet.searchByName);
+router.post('/pet', pet.create);
+router.put('/pet/:codigo_pet', pet.update);
+router.delete('/pet/:codigo_pet', pet.delete);
+
+
+export {router}
